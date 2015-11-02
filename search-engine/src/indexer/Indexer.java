471bac2e30df0385cedb49f9a645131a6e1bf6f8
Jalpan Randeri <jalpaan.randeri@gmail.com>
2015-11-02 01:47:10 -0500

    Indexer
    Implemented Indexer which create inverted indices in form of
    term -> List of {doc_id, term_freq}
    Indexer program generates the file which is inverted indices

--- search-engine/src/indexer/Indexer.java
+++ search-engine/src/indexer/Indexer.java
@@ -1,7 +1,121 @@
 package indexer;
 
+import model.DocumentModel;
+import utils.FileUtils;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Indexer will generate inverted indices for search engine
  */
 public class Indexer {
+    public static void main(String[] args) throws IOException {
+
+        if(args.length == 2) {
+            Map<String, List<DocumentModel>> invertedIndex = getInvertedIndex(args[0]);
+            invertedIndex.entrySet().forEach(entry -> {
+                        String line = getFlattern(entry);
+                        try {
+                            FileUtils.appendToFile(args[1], line);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+            );
+        }else{
+            System.out.println("Usage: inputfile outputfile");
+        }
+    }
+
+
+    private static String getFlattern(Map.Entry<String, List<DocumentModel>> entry){
+        return String.format("%s %s",entry.getKey(), entry.getValue());
+    }
+
+
+
+    private static Map<String, List<DocumentModel>> getInvertedIndex(String inputFile) throws IOException {
+        HashMap<String, List<DocumentModel>> cache = new HashMap<>();
+        Map<String, Long> termFrequencyCache = getTermFrequency(FileUtils.readFiles(inputFile));
+
+        Iterator<String> reader = FileUtils.readFiles(inputFile).iterator();
+        String lastDocId = null;
+        while(reader.hasNext()){
+            String line = reader.next();
+
+            if(isDocument(line)){
+                lastDocId = line.trim();
+
+            }else if(lastDocId != null){
+                DocumentModel document = new DocumentModel(lastDocId, termFrequencyCache.get(lastDocId));
+
+                List<String> terms = getAllTerms(line);
+
+                for(String term : terms){
+                    List<DocumentModel> list;
+                    if(cache.containsKey(term)){
+                        list = cache.get(term);
+                    }else {
+                        list = new ArrayList<>();
+                    }
+                    list.add(document);
+                    cache.put(term, list);
+                }
+
+            }
+        }
+
+        return cache;
+    }
+
+
+    private static List<String> getAllTerms(String line){
+        return Arrays.stream(line.split(" "))
+                     .filter(Indexer::isWord)
+                     .collect(Collectors.toList());
+    }
+
+
+    private static Map<String, Long> getTermFrequency(Stream<String> docTerms){
+        HashMap<String, Long> cacheTermFrequency = new HashMap<>();
+
+        Iterator<String> reader = docTerms.iterator();
+        String lastDoc = null;
+        while(reader.hasNext()){
+            String line = reader.next();
+
+            if(isDocument(line)){
+                // add document with 0 term
+                lastDoc = line.trim();
+                cacheTermFrequency.put(lastDoc, 0L);
+
+            }else if(lastDoc != null) {
+                long count = getTotalTermsInDocument(line);
+                cacheTermFrequency.put(lastDoc, cacheTermFrequency.get(lastDoc) + count);
+            }
+        }
+
+        return cacheTermFrequency;
+    }
+
+    private static boolean isDocument(String line) {
+        return line.startsWith("#");
+    }
+
+    private static long getTotalTermsInDocument(String line) {
+        return Arrays.stream(line.split(" "))
+                     .filter(Indexer::isWord)
+                     .count();
+    }
+
+    private static boolean isWord(String word){
+        return !isDigit(word);
+    }
+
+    private static boolean isDigit(String word) {
+        return word.chars().allMatch(c ->  c >= '0' && c <= '9');
+    }
 }
--- search-engine/src/utils/FileUtils.java
+++ search-engine/src/utils/FileUtils.java
@@ -1,12 +1,10 @@
 package utils;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * File utilities
@@ -16,12 +14,12 @@ public class FileUtils {
     /**
      * read content of file
      * @param fileName String file name
-     * @return List of string
+     * @return Stream of string
      * @throws IOException if file is found or accessible
      */
-    public List<String> readFiles(String fileName) throws IOException {
-        return  Files.lines(Paths.get(fileName, "UTF-8"))
-                     .collect(Collectors.toList());
+    public static Stream<String> readFiles(String fileName) throws IOException {
+        return  Files.lines(Paths.get(fileName));
+
     }
 
     /**
@@ -30,10 +28,22 @@ public class FileUtils {
      * @param content List of string as contnet
      * @throws FileNotFoundException if file is not accessible
      */
-    public void writeFile(String fileName, List<String> content) throws FileNotFoundException {
+    public static void writeFile(String fileName, List<String> content) throws FileNotFoundException {
         PrintWriter writer = new PrintWriter(fileName);
         content.stream()
                .forEach(writer::println);
+        writer.close();
+    }
+
+    /**
+     * add line to file
+     * @param filename String file name
+     * @param content  String content line
+     */
+    public static void appendToFile(String filename, String content) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true));
+        writer.newLine();
+        writer.append(content);
         writer.close();
     }
 }

dfcc44726cc0b1eabe050dcd11d0aa734f5fbab5
Jalpan Randeri <jalpaan.randeri@gmail.com>
2015-11-02 10:52:29 -0500

    Documented

---
+++ search-engine/index.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+read INPUT
+read OUTPUT
+java out/indexer.Indexer INPUT OUTPUT
--- search-engine/src/indexer/Indexer.java
+++ search-engine/src/indexer/Indexer.java
@@ -16,27 +16,36 @@ public class Indexer {
 
         if(args.length == 2) {
             Map<String, List<DocumentModel>> invertedIndex = getInvertedIndex(args[0]);
-            invertedIndex.entrySet().forEach(entry -> {
-                        String line = getFlattern(entry);
+            invertedIndex.entrySet()
+                    .forEach(entry -> {
+                        String line = getFlatten(entry);
                         try {
                             FileUtils.appendToFile(args[1], line);
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-                    }
-            );
+                    });
         }else{
-            System.out.println("Usage: inputfile outputfile");
+            System.out.println("Usage: input_file output_file");
         }
     }
 
-
-    private static String getFlattern(Map.Entry<String, List<DocumentModel>> entry){
+    /**
+     * flatten the entry in map
+     * @param entry Map.Entry {docId, inverted index}
+     * @return String representation of entry
+     */
+    private static String getFlatten(Map.Entry<String, List<DocumentModel>> entry){
         return String.format("%s %s",entry.getKey(), entry.getValue());
     }
 
 
-
+    /**
+     * build inverted index from stemmed documents collection
+     * @param inputFile String input file
+     * @return Map of Term, List of Document and Frequency
+     * @throws IOException when input file is not accessible
+     */
     private static Map<String, List<DocumentModel>> getInvertedIndex(String inputFile) throws IOException {
         HashMap<String, List<DocumentModel>> cache = new HashMap<>();
         Map<String, Long> termFrequencyCache = getTermFrequency(FileUtils.readFiles(inputFile));
@@ -71,14 +80,23 @@ public class Indexer {
         return cache;
     }
 
-
+    /**
+     * get all terms
+     * @param line String line
+     * @return list of term
+     */
     private static List<String> getAllTerms(String line){
         return Arrays.stream(line.split(" "))
-                     .filter(Indexer::isWord)
+                     .filter(Indexer::isNotNumber)
                      .collect(Collectors.toList());
     }
 
 
+    /**
+     * get term frequency
+     * @param docTerms Stream of document terms where line starts with # contains docId and followed by terms
+     * @return Map of DocId, TermCount
+     */
     private static Map<String, Long> getTermFrequency(Stream<String> docTerms){
         HashMap<String, Long> cacheTermFrequency = new HashMap<>();
 
@@ -101,20 +119,40 @@ public class Indexer {
         return cacheTermFrequency;
     }
 
+    /**
+     * checks validity of documents
+     * @param line String line
+     * @return true if given document line starts with #
+     */
     private static boolean isDocument(String line) {
         return line.startsWith("#");
     }
 
+    /**
+     * total terms count in given document
+     * @param line String document line
+     * @return long count of all terms
+     */
     private static long getTotalTermsInDocument(String line) {
         return Arrays.stream(line.split(" "))
-                     .filter(Indexer::isWord)
+                     .filter(Indexer::isNotNumber)
                      .count();
     }
 
-    private static boolean isWord(String word){
+    /**
+     * is not Number
+     * @param word String word
+     * @return true if given word is not number
+     */
+    private static boolean isNotNumber(String word){
         return !isDigit(word);
     }
 
+    /**
+     * checks for given word contains only digits ?
+     * @param word String word
+     * @return true if all characters in word are digit
+     */
     private static boolean isDigit(String word) {
         return word.chars().allMatch(c ->  c >= '0' && c <= '9');
     }

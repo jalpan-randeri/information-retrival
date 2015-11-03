f40970a64d8ffa43f6c154732801b94aa4ae38b5
Jalpan Randeri <jalpaan.randeri@gmail.com>
2015-11-03 15:30:42 -0500

    modified to match profs steps

--- search-engine/src/ranker/BM25Ranker.java
+++ search-engine/src/ranker/BM25Ranker.java
@@ -31,7 +31,7 @@ public class BM25Ranker {
                       List<DocumentScore> scoreList = entry.getValue();
 
                       scoreList.stream()
-                              .limit(100)
+                              .limit(10)
                               .forEach(doc -> System.out.printf("%s  %s  %f\n",query, doc.getDocumentId(), doc.getScore()));
 
                   });
@@ -41,8 +41,16 @@ public class BM25Ranker {
 
     }
 
-    private static Map<String, List<DocumentScore>> getQueryRank(String queryDocument, String invertedIndexFile) throws IOException {
 
+    /**
+     * get ranked documents for queries
+     * @param queryDocument String query document file
+     * @param invertedIndexFile String inverted index file
+     * @return Map of Query and list of ranked document in decreasing sorted fashion
+     * @throws IOException
+     */
+    private static Map<String, List<DocumentScore>> getQueryRank(String queryDocument, String invertedIndexFile) throws IOException {
+        // get all documents and their respective length
         Map<String, Long> documentLength = getAllDocumentsAndLength(invertedIndexFile);
         double avgDocumentLength = documentLength.values()
                                         .parallelStream()
@@ -51,10 +59,9 @@ public class BM25Ranker {
                                         .getAsDouble();
 
 
+        Map<String, List<DocumentScore>> queryDocumentScores = new LinkedHashMap<>();
         Stream<String> queries = FileUtils.readFiles(queryDocument);
         Iterator<String> iterator = queries.iterator();
-        Map<String, List<DocumentScore>> queryDocumentScores = new HashMap<>();
-
         while(iterator.hasNext()){
             String query = iterator.next();
             List<DocumentScore> rankedDocuments = getRank(query, invertedIndexFile, documentLength, avgDocumentLength);
@@ -66,6 +73,11 @@ public class BM25Ranker {
     }
 
 
+    /**
+     * get all documents and their respective length
+     * @param invertedIndexFile String inverted index file
+     * @return Map of Document -> length
+     */
     private static Map<String, Long> getAllDocumentsAndLength(String invertedIndexFile){
 
         HashMap<String, Long> documentLength = new HashMap<>();
@@ -77,6 +89,7 @@ public class BM25Ranker {
             while(iterator.hasNext()){
                 String line = iterator.next();
 
+                // skip till the separator
                 if(line.equals(Indexer.DOC_LENGTH_SEPRATOR)) {
                     // read all docs and length
                     while(iterator.hasNext()) {
@@ -85,9 +98,7 @@ public class BM25Ranker {
                         documentLength.put(tokens[0], Long.parseLong(tokens[1]));
                     }
                 }
-
             }
-
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -97,44 +108,76 @@ public class BM25Ranker {
     }
 
 
-
+    /**
+     * get ranked document for query
+     * @param query String query
+     * @param invertedIndexFile String inverted index file
+     * @param documentLength Map of Document -> Length
+     * @param avgDocumentLength Double average document length
+     * @return List of scored document length sorted in decreasing fashion
+     */
     private static List<DocumentScore> getRank(String query,
                                                String invertedIndexFile,
                                                Map<String, Long> documentLength,
                                                double avgDocumentLength){
-
+        // get term frequency inside query
         Map<String, Long> queryTermFrequency = getQueryTermFrequency(query);
+        // total document length
         int N = documentLength.size();
         Long R = 0L;  // relevance is not available
         Long ri = 0L; // relevance is not available
 
+
+
+        // compute score
+        List<List<DocumentScore>> termDocumentScores = queryTermFrequency
+                .entrySet()
+                .stream()
+                .map(queryTerm -> {
+
+                    String term = queryTerm.getKey();
+                    long qfi = queryTerm.getValue();
+
+                    // get inverted index for the term
+                    InvertedIndex invertedIndex = getInvertedIndex(term, invertedIndexFile).get();
+
+                    // get all documents from inverted index
+                    Map<String, Long> docTermFreq = invertedIndex.getDocumentFrequency();
+                    int ni = invertedIndex.getDocumentFrequency().keySet().size();
+
+                    // calculate score for each term
+                    List<DocumentScore> documentScores = docTermFreq.entrySet()
+                                                            .stream()
+                                                            .map(docEntry -> {
+                                                                    String docName = docEntry.getKey();
+
+                                                                long fi = docEntry.getValue();
+                                                                long dl = documentLength.get(docName);
+
+                                                                double currentScore = computeBM25Score(N, R, ni, ri, fi, qfi, K1, K2, B, dl, avgDocumentLength);
+
+                                                                return new DocumentScore(docName, currentScore);
+                                                            }).collect(Collectors.toList());
+                    return documentScores;
+                }).collect(Collectors.toList());
+
+
         HashMap<String, Double> scores = new HashMap<>();
+        termDocumentScores
+                .stream()
+                .forEach(scoredDocList -> {
+                    scoredDocList
+                            .stream()
+                            .forEach(score -> {
+                                if(scores.containsKey(score.getDocumentId())){
+                                    scores.put(score.getDocumentId(),
+                                               scores.get(score.getDocumentId()) + score.getScore());
+                                }else{
+                                    scores.put(score.getDocumentId(), score.getScore());
+                                }
+                            });
+                });
 
-        queryTermFrequency.entrySet().stream().forEach(queryTerm -> {
-            String term = queryTerm.getKey();
-            long qfi = queryTerm.getValue();
-
-            InvertedIndex invertedIndex = getInvertedIndex(term, invertedIndexFile).get();
-            Map<String, Long> docTermFreq = invertedIndex.getDocumentFrequency();
-            int ni = invertedIndex.getDocumentFrequency().size();
-
-            docTermFreq.entrySet().stream().forEach(docEntry ->{
-                String docName = docEntry.getKey();
-                long fi = docEntry.getValue();
-                long dl = documentLength.get(docName);
-
-                double currentScore = computeBM25Rank(N, R, ni, ri, fi, qfi, K1, K2, B, dl, avgDocumentLength);
-
-                // check and add previously computed score with current score
-                if(scores.containsKey(docName)){
-                    scores.put(docName, scores.get(docName) + currentScore);
-                }else{
-                    scores.put(docName, currentScore);
-                }
-
-            });
-
-        });
 
 
         return scores.entrySet()
@@ -145,11 +188,23 @@ public class BM25Ranker {
     }
 
 
-
-
-
-    private static double computeBM25Rank(double N, double R, double ni, double ri, double fi, double qfi, double k1,
-                                                       double k2, double b, long dl, double avdl){
+    /**
+     * compute BM25 score
+     * @param N total number of document in collection
+     * @param R total number of relevant documents in collection
+     * @param ni total number of document containing ith term
+     * @param ri total number of relevant document containing ith term
+     * @param fi frequency of ith term in document
+     * @param qfi frequency of ith term in query
+     * @param k1 parameter
+     * @param k2 parameter
+     * @param b parameter
+     * @param dl document length
+     * @param avdl average document length
+     * @return double BM25 score
+     */
+    private static double computeBM25Score(double N, double R, double ni, double ri, double fi, double qfi, double k1,
+                                           double k2, double b, long dl, double avdl){
         // find term frequency in query
         // calculate score for all documents inside document list
         // Note:
@@ -193,7 +248,14 @@ public class BM25Ranker {
 
     }
 
-
+    /**
+     * compute K parameter
+     * @param k1 parameter
+     * @param b parameter
+     * @param documentLength Double
+     * @param avgDocumentLength Double
+     * @return Parameter K
+     */
     private static double computeK(double k1, double b, double documentLength, double avgDocumentLength){
         /**
          *                             dl
@@ -208,6 +270,11 @@ public class BM25Ranker {
     }
 
 
+    /**
+     * get query term frequency
+     * @param query String query
+     * @return Map of Term -> Frequency inside query
+     */
     private static Map<String, Long> getQueryTermFrequency(String query) {
         LinkedHashMap<String, Long> queryTermFrequency = new LinkedHashMap<>();
         for(String term : query.split(SEPARATOR_SPACE)){
@@ -221,7 +288,12 @@ public class BM25Ranker {
         return queryTermFrequency;
     }
 
-
+    /**
+     * retrieve inverted index
+     * @param term String term
+     * @param invertedIndexFile String inverted index file
+     * @return may be inverted index if index is present in collection
+     */
     private static Optional<InvertedIndex> getInvertedIndex(String term, String invertedIndexFile)  {
         try {
             Stream<String> invertedIndex = FileUtils.readFiles(invertedIndexFile);
